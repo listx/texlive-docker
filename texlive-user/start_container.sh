9ddaef2156c21b17eebf7d7d26605b1b793dd079
Linus Arver <linusarver@gmail.com>
2021-01-21 09:46:38 -0800

    start_container: make idempotent

--- texlive-user/start_container.sh
+++ texlive-user/start_container.sh
@@ -7,4 +7,13 @@ set -o xtrace
 
 __user=$(id -u -n)
 
-docker run -d --rm -v /home:/home --name texlive2020-${__user} listx/texlive:2020-${__user}
+__container_name=texlive2020-${__user}
+
+__container_id="$(docker ps -f name=${__container_name} -q)"
+
+# Only run a container if it isn't already running.
+if [[ -n "${__container_id}" ]]; then
+    echo "${__container_name} is already running"
+else
+    docker run -d --rm -v /home:/home --name ${__container_name} listx/texlive:2020-${__user}
+fi
--- texlive/start_container.sh
+++ texlive/start_container.sh
@@ -5,4 +5,13 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-docker run -d --rm -v /home:/home --name texlive2020-root listx/texlive:2020
+__container_name=texlive2020-root
+
+__container_id="$(docker ps -f name=${__container_name} -q)"
+
+# Only run a container if it isn't already running.
+if [[ -n "${__container_id}" ]]; then
+    echo "${__container_name} is already running"
+else
+    docker run -d --rm -v /home:/home --name ${__container_name} listx/texlive:2020
+fi
