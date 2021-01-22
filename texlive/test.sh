fd41a4fa56736f66becdf01080020eb257b5dc1d
Linus Arver <linusarver@gmail.com>
2021-01-22 09:14:44 -0800

    tests: cd into PROJECT_ROOT first
    This makes things more consistent and simpler to reason about.

--- texlive/test.sh
+++ texlive/test.sh
@@ -8,7 +8,7 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 # Ensure we can build with the basic container.
 "${PROJECT_ROOT}"/texlive/start_container.sh
-"${PROJECT_ROOT}"/texlive/with_container.sh "latexmk -pdf ${PROJECT_ROOT}/test/minimal.tex"
+"${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT}/test && pdflatex minimal.tex"
 if [[ ! -f ${PROJECT_ROOT}/test/minimal.pdf ]]; then
     echo >&2 "could not compile ${PROJECT_ROOT}/test/minimal.pdf"
     exit 1
@@ -17,7 +17,7 @@ fi
 # Test that latexindent dependencies can be found.
 "${PROJECT_ROOT}"/texlive/with_container.sh "${PROJECT_ROOT}/test/latexindent-dependencies.pl"
 # Test that latexindent can run against minimal.tex.
-"${PROJECT_ROOT}"/texlive/with_container.sh "latexindent ${PROJECT_ROOT}/test/minimal.tex"
+"${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT}/test && latexindent minimal.tex"
 
 # Cleanup.
-"${PROJECT_ROOT}"/texlive/with_container.sh "${PROJECT_ROOT}/test/clean.sh"
+"${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT}/test && ./clean.sh"
