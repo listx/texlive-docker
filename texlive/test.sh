d91e38db6008cb2a4fd8857dc205ebfe725bf6b0
Linus Arver <linusarver@gmail.com>
2021-01-21 23:20:32 -0800

    add tests for checking latexindent behavior

--- texlive/test.sh
+++ texlive/test.sh
@@ -14,8 +14,10 @@ if [[ ! -f ${PROJECT_ROOT}/test/minimal.pdf ]]; then
     exit 1
 fi
 
+# Test that latexindent dependencies can be found.
+"${PROJECT_ROOT}"/texlive/with_container.sh "${PROJECT_ROOT}/test/latexindent-dependencies.pl"
 # Test that latexindent can run against minimal.tex.
-"${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT}/test && latexindent minimal.tex"
+"${PROJECT_ROOT}"/texlive/with_container.sh "latexindent ${PROJECT_ROOT}/test/minimal.tex"
 
 # Cleanup.
 "${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT} && ./test/clean.sh"
