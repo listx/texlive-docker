9c184c3df55ac0171147d3277eb98e7fcd296483
Linus Arver <linusarver@gmail.com>
2021-01-21 23:10:24 -0800

    skip explicit installation of latexindent dependencies
    latexindent runs fine without having to install these Perl modules
    (probably fixed as a result of TexLive 2020).

--- texlive/test.sh
+++ texlive/test.sh
@@ -14,5 +14,8 @@ if [[ ! -f ${PROJECT_ROOT}/test/minimal.pdf ]]; then
     exit 1
 fi
 
+# Test that latexindent can run against minimal.tex.
+"${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT}/test && latexindent minimal.tex"
+
 # Cleanup.
 "${PROJECT_ROOT}"/texlive/with_container.sh "cd ${PROJECT_ROOT} && ./test/clean.sh"
